247436e98f12c69b3b3ecadf5938aa6d01eda667
sje08250 <sje08250@gmail.com>
2023-11-17 16:52:39 +0900

    products

---
+++ front/src/components/DepositProductList.vue
@@ -0,0 +1,37 @@
+<template>
+  <div @click="goDetail">
+    <h1>예금 상품</h1>
+    <p>공시 제출월 : {{ product.dcls_month }}</p>
+    <p>금융회사명 : {{ product.kor_co_nm }}</p>
+    <p>상품명 : {{ product.fin_prdt_nm }}</p>
+    <p>{{ product }}</p>
+    <!-- <ProductDetailView
+      :dep_product="product"
+    /> -->
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useProductStore } from '@/stores/product'
+import { useRouter, useRoute } from 'vue-router'
+import ProductDetailView from '@/views/ProductDetailView.vue'
+
+const store = useProductStore()
+const router = useRouter()
+const route = useRoute()
+
+const props = defineProps({
+  product: Object
+})
+
+const goDetail = function () {
+  router.push({ name: 'ProductDetail', params: { type: 'dep', productId: props.product.fin_prdt_cd } })
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
--- front/src/views/ProductDetailView.vue
+++ front/src/views/ProductDetailView.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
     <h1>Product Detail View</h1>
-    <p>jieun edit</p>
+    <p v-for="option in store.optionsValue">
+      <p>공시 제출월 : {{ option }}</p>
+      <p>금융회사명 : {{ option }}</p>
+      <p>상품명 : {{ option }}</p>
+      <p>가입제한 : {{ option }}</p>
+      <p>가입 방법 : {{ option }}</p>
+      <p>우대조건 : {{ option }}</p>
+    </p>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
+import { useProductStore } from '@/stores/product'
+import { useRoute } from 'vue-router';
 
+const route = useRoute()
+const store = useProductStore()
+
+onMounted(() => {
+  if (route.params.type === 'dep') {
+    store.getDepOptions(route.params.productId)
+  } else if (route.params.type === 'sav') {
+    store.getSavOptions(route.params.productId)
+  }
+})
 </script>
 
 <style scoped>
