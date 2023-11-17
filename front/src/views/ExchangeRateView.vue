99f23cc9912b578ac66ed49aedae34352b4b92e8
onassisryu <onyourown793@gmail.com>
2023-11-17 14:54:53 +0900

    exchange page

--- front/src/views/ExchangeRateView.vue
+++ front/src/views/ExchangeRateView.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <h1>Exchange Rate View</h1>
+    <ExchangeRateCalculator />
   </div>
 </template>
 
 <script setup>
+import ExchangeRateCalculator from '@/components/ExchangeRateCalculator.vue'
 
 </script>
 
