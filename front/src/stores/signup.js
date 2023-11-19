c379a5c4f7d139832e89892051bab157c9253ffe
onass <onyourown793@gmail.com>
2023-11-19 14:44:39 +0900

    signup 오류

--- front/src/stores/signup.js
+++ front/src/stores/signup.js
@@ -7,23 +7,27 @@ export const useSignupStore = defineStore('signup', () => {
   const router = useRouter()
   const API_URL = 'http://127.0.0.1:8000'
 
-  const signUp = function (payload) {
+  const signUp = function (formData) {
     axios({
       method: 'post',
       url: `${API_URL}/dj-rest-auth/registration/`,
-      data: {
-        username: payload.username,
-        password1: payload.password1, 
-        password2: payload.password2,
-        nickname: payload.nickname,
-        // profile_thumbnail: payload.profileImg,
-        age: payload.age,
-        // email: payload.email,
-        // money: payload.money,
-        // salary: payload.salary,
-        // financial_products: payload.financialProducts,
-        // mbti: payload.mbti,
-      }
+      data: formData,
+      // {
+      //   username: payload.username,
+      //   password1: payload.password1, 
+      //   password2: payload.password2,
+      //   nickname: payload.nickname,
+      //   profile_thumbnail: payload.profileImg,
+      //   age: payload.age,
+      //   email: payload.email,
+      //   money: payload.money,
+      //   salary: payload.salary,
+      //   financial_products: payload.financialProducts,
+      //   mbti: payload.mbti,
+      // }
+      headers: {
+        'Content-Type': 'multipart/form-data', // 파일 업로드를 위한 콘텐츠 타입 설정
+      },
     })
       .then((res) => {
         console.log(res)
--- front/src/views/SignUpView.vue
+++ front/src/views/SignUpView.vue
@@ -14,7 +14,8 @@
       <label for="nickname" style="color: red;">* nickname: </label>
       <input type="text" id="nickname" v-model.trim="nickname">
       <label for="profile-img">profileImg: </label>
-      <input type="text" id="profile-img" v-model.trim="profileImg">
+      <input type="file" id="profile-img" ref="profileImgInput" @change="handleProfileImageChange">
+      <!-- <input type="text" id="profile-img" v-model.trim="profileImg"> -->
       <label for="age" style="color: red;">* age: </label>
       <input type="text" id="age" v-model.trim="age">
       <label for="email">email: </label>
@@ -25,10 +26,43 @@
       <input type="text" id="money" v-model.trim="money">
       <label for="salary">salary: </label>
       <input type="text" id="salary" v-model.trim="salary">
-      <label for="financial-products">financial products: </label>
-      <input type="text" id="financial-products" v-model.trim="financialProducts">
+      <label for="financial-products">financial products: {{ financialProducts }} </label>
+      <div>
+        <input type="checkbox" id="예금" value="예금" v-model="financialProducts"> <label for="예금">예금</label>
+      </div>
+      <div>
+        <input type="checkbox" id="적금" value="적금" v-model="financialProducts"> <label for="적금">적금</label>
+      </div>
+      <!-- <input type="text" id="financial-products" v-model.trim="financialProducts"> -->
+      <label for="favorite">favorite: {{ favorite }} </label>
+      <div>
+        <input type="checkbox" id="전자기기" value="전자기기" v-model="favorite"> <label for="전자기기">전자기기</label>
+      </div>
+      <div>
+        <input type="checkbox" id="영화" value="영화" v-model="favorite"> <label for="영화">영화</label>
+      </div>
+      <div>
+        <input type="checkbox" id="패션" value="패션" v-model="favorite"> <label for="패션">패션</label>
+      </div>
       <label for="mbti">mbti: </label>
-      <input type="text" id="mbti" v-model.trim="mbti">
+      <select id="mbti" v-model="mbti">
+        <option value="ISTJ">ISTJ</option>
+        <option value="ISFJ">ISFJ</option>
+        <option value="INFJ">INFJ</option>
+        <option value="INTJ">INTJ</option>
+        <option value="ISTP">ISTP</option>
+        <option value="ISFP">ISFP</option>
+        <option value="INFP">INFP</option>
+        <option value="INTP">INTP</option>
+        <option value="ESTP">ESTP</option>
+        <option value="ESFP">ESFP</option>
+        <option value="ENFP">ENFP</option>
+        <option value="ENTP">ENTP</option>
+        <option value="ESTJ">ESTJ</option>
+        <option value="ESFJ">ESFJ</option>
+        <option value="ENFJ">ENFJ</option>
+        <option value="ENTJ">ENTJ</option>
+      </select>
       
       <input type="submit">
     </form>
@@ -46,29 +80,79 @@ const password1 = ref(null)
 const password2 = ref(null)
 const nickname = ref(null)
 const profileImg = ref(null)
+const profileImgInput = ref(null)
 const age = ref(null)
 const email = ref(null)
 const money = ref(null)
 const salary = ref(null)
-const financialProducts = ref(null)
+const financialProducts = ref([])
+// const financialProducts = ref(null)
+const favorite = ref([])
 const mbti = ref(null)
 
-const signUp = function () {
-  const payload = {
-    username: username.value,
-    password1: password1.value,
-    password2: password2.value,
-    nickname: nickname.value,
-    profileImg: profileImg.value,
-    age: age.value,
-    email: email.value,
-    money: money.value,
-    salary: salary.value,
-    financialProducts: financialProducts.value,
-    mbti: mbti.value,
+const handleProfileImageChange = function () {
+  const file = profileImgInput.value.files[0]
+  if (file) {
+    // 필요한 경우 여기서 이미지 미리보기 또는 기타 작업을 처리 가능
+    profileImg.value = file
   }
-  store.signUp(payload)
 }
+
+
+const signUp = function () {
+  const formData = new FormData()
+  formData.append('username', username.value)
+  formData.append('password1', password1.value)
+  formData.append('password2', password2.value)
+  formData.append('nickname', nickname.value)
+  formData.append('age', age.value)
+  formData.append('email', email.value)
+  formData.append('money', money.value)
+  formData.append('salary', salary.value)
+  // const financialProductsInt = financialProducts.value.map(value => parseInt(value))
+  // formData.append('financial_products', JSON.stringify(financialProductsInt))
+  // const favoriteInt = favorite.value.map(value => parseInt(value))
+  // formData.append('favorite', JSON.stringify(favoriteInt))
+  // formData.append('financial_products', financialProducts.value.join(','))
+  // formData.append('financial_products', financialProducts.value)
+  // financialProducts.value.forEach((value, index) => {
+  //   formData.append(`financial_products[${index}]`, value)
+  // })
+  // formData.append('favorite', favorite.value.join(','))
+  financialProducts.value.forEach((item, index) => {
+    formData.append(`financial_products[${index}]`, item);
+  })
+  favorite.value.forEach((item, index) => {
+    formData.append(`favorite[${index}]`, item);
+  });
+  
+  formData.append('mbti', mbti.value)
+
+  
+  if (profileImg.value) {
+    formData.append('profile_thumbnail', profileImg.value)
+  }
+
+
+  store.signUp(formData)
+}
+
+// const signUp = function () {
+//   const payload = {
+//     username: username.value,
+//     password1: password1.value,
+//     password2: password2.value,
+//     nickname: nickname.value,
+//     profileImg: profileImg.value, 
+//     age: age.value,
+//     email: email.value,
+//     money: money.value,
+//     salary: salary.value,
+//     financialProducts: financialProducts.value,
+//     mbti: mbti.value,
+//   }
+//   store.signUp(payload)
+// }
 
 </script>
 
