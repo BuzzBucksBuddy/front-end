df7d52d6b5722447919a325ff9ddf407dbdd9832
junsukim <julsukim96@gmail.com>
2023-11-21 16:17:44 +0900

    front

--- front/src/views/BankMapView.vue
+++ front/src/views/BankMapView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Bank Map View</h1>
+    <h1>은행 찾기</h1>
     <select name="dos" id="do" v-model="selectDo">
       <option :value="null" selected disabled>--선택하세요--</option>
       <option v-for="dos in store.doList" :key="dos.code" :value="dos.code">{{ dos.name }}</option>
@@ -13,33 +13,37 @@
       <option value="은행" selected="selected">모든 은행</option>
       <option v-for="bank in articleStore.bankCategories" :key="bank" :value="bank.name">{{ bank.name }}</option>
     </select>
-    <section class="map-container">
-      <BankMap :select-gu="selectGu" :select-bank="selectBank"/>
-    </section>
-    <div class="selector">
-      <h2>{{ selectBank }}</h2>
-      <h3 :class="{ selected: switcher}" @click="switcher=true">예금</h3>
-      <h3 :class="{ selected: !switcher}" @click="switcher=false">적금</h3>
-    </div>
-    <section v-show="switcher">
-      <div v-if="depLen">
-        <BankDepositProductList
-          v-for="product in store.depList"
-          :key="product.id"
-          :products="product"
-        />
+    <section class="container">
+      <div class="map-container">
+        <BankMap :select-gu="selectGu" :select-bank="selectBank"/>
       </div>
-      <div v-else>해당 은행의 예금 상품이 없습니다</div>
-    </section>
-    <section v-show="!switcher">
-      <div v-if="savLen">
-        <BankSavingProductList
-          v-for="product in store.savList"
-          :key="product.id"
-          :products="product"
-        />
+      <div class="bank-container">
+        <div class="selector">
+          <h2>{{ selectBank }}</h2>
+          <h3 :class="{ selected: switcher}" @click="switcher=true">예금</h3>
+          <h3 :class="{ selected: !switcher}" @click="switcher=false">적금</h3>
+        </div>
+        <section class="bank-items" v-show="switcher">
+          <div v-if="depLen">
+            <BankDepositProductList
+              v-for="product in store.depList"
+              :key="product.id"
+              :products="product"
+            />
+          </div>
+          <div v-else>해당 은행의 예금 상품이 없습니다</div>
+        </section>
+        <section v-show="!switcher">
+          <div v-if="savLen">
+            <BankSavingProductList
+              v-for="product in store.savList"
+              :key="product.id"
+              :products="product"
+            />
+          </div>
+          <div v-else>해당 은행의 적금 상품이 없습니다</div>
+        </section>
       </div>
-      <div v-else>해당 은행의 적금 상품이 없습니다</div>
     </section>
   </div>
 </template>
@@ -99,10 +103,25 @@ const bankWatcher = watch(() => selectBank.value, (newValue, old) => {
 </script>
 
 <style scoped>
-.map-container {
-  width: 100%;
-  height: 400px;
+.container {
+  display: flex;
+  height: 700px;
+  gap: 20px;
   margin-top: 20px;
+}
+.map-container {
+  width: 50%;
+  height: 100%;
+}
+.bank-container {
+  height: 100%;
+  width: 50%;
+  padding: 0;
+}
+.bank-items {
+  height: 100%;
+  overflow-y: scroll;
+  padding: 0 10px;
 }
 .selected {
   color: rgb(242, 185, 60);
@@ -110,7 +129,7 @@ const bankWatcher = watch(() => selectBank.value, (newValue, old) => {
 .selector {
   display: flex;
   align-items: baseline;
-  margin: 10px 0
+  border-bottom: 1px solid;
 }
 .selector h3 {
   margin-left: 10px;
